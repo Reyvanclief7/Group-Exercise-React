6da0245dc4dee16a4a97d6a5e4d7cf818e4bd7d5
Reyvan clief <rey17van17@gmail.com>
2024-11-01 11:57:38 +0800

    add Home & Search

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-const App = () => {
-  const hello = "Hello World";
+import React from 'react';
+import Header from './components/Header/Header';
+import Home from './components/Home/Home';
+import Footer from './components/Footer/Footer';
+
+function App() {
   return (
-    <>
-      <h1>test</h1>
-      <h2>{hello}</h2>
-    </>
+    <div>
+      <Header />
+      <Home />
+      <Footer />
+    </div>
   );
-};
+}
+
 export default App;
---
+++ src/componets/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Search from './Search';
+import CardInfo from './CardInfo/CardInfo';
+
+function Home() {
+  return (
+    <main>
+      <Search />
+      <CardInfo />
+    </main>
+  );
+}
+
+export default Home;
